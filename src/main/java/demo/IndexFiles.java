8a9f6f9f2e22c6e144d234f205d0d7ae4b0e1df1
Haitao Hu <lakehaitao@gmail.com>
2022-01-12 00:16:54 +0800

    test set ram size

--- src/main/java/demo/IndexFiles.java
+++ src/main/java/demo/IndexFiles.java
@@ -86,7 +86,7 @@ public class IndexFiles {
             // buffer.  But if you do this, increase the max heap
             // size to the JVM (eg add -Xmx512m or -Xmx1g):
             //
-            // iwc.setRAMBufferSizeMB(256.0);
+            iwc.setRAMBufferSizeMB(256.0);
 
             IndexWriter writer = new IndexWriter(dir, iwc);
             indexDocs(writer, docDir);
