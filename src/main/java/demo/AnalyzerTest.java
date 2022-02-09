6c777ff3e6dd9df2ba6db603e5405400aa608079
Haitao Hu <lakehaitao@gmail.com>
2022-02-10 00:57:13 +0800

    add analyzer test

---
+++ src/main/java/demo/AnalyzerTest.java
@@ -0,0 +1,35 @@
+package demo;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+public class AnalyzerTest {
+    public static void main(String[] args) throws IOException {
+        Analyzer analyzer = new StandardAnalyzer(); // or any other analyzer
+
+        TokenStream ts = analyzer.tokenStream("myfield", new StringReader("some text goes here"));
+        // The Analyzer class will construct the Tokenizer, TokenFilter(s), and CharFilter(s),
+        //   and pass the resulting Reader to the Tokenizer.
+        OffsetAttribute offsetAtt = ts.addAttribute(OffsetAttribute.class);
+
+        try {
+            ts.reset(); // Resets this stream to the beginning. (Required)
+            while (ts.incrementToken()) {
+                // Use {@link org.apache.lucene.util.AttributeSource#reflectAsString(boolean)}
+                // for token stream debugging.
+                System.out.println("token: " + ts.reflectAsString(true));
+
+                System.out.println("token start offset: " + offsetAtt.startOffset());
+                System.out.println("  token end offset: " + offsetAtt.endOffset());
+            }
+            ts.end();   // Perform end-of-stream operations, e.g. set the final offset.
+        } finally {
+            ts.close(); // Release resources associated with this stream.
+        }
+    }
+}
